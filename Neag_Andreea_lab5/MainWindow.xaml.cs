ec1e3a84865cc4bef8894d722c8f82dc3982d172
Andreea Neag <neag_andreea@yahoo.com>
2021-11-21 14:18:50 +0200

    laborator7

--- Neag_Andreea_lab5/MainWindow.xaml.cs
+++ Neag_Andreea_lab5/MainWindow.xaml.cs
@@ -71,18 +71,24 @@ namespace Neag_Andreea_Lab5
             cmbInventory.ItemsSource = ctx.Inventories.Local;
             //cmbInventory.DisplayMemberPath = "Make";
             cmbInventory.SelectedValuePath = "CarId";
-
             BindDataGrid();
         }
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             action = ActionState.New;
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            SetValidationBinding();
+
         }
 
         private void btnEdit_Click(object sender, RoutedEventArgs e)
         {
             action = ActionState.Edit;
+            BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+            BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+            SetValidationBinding();
         }
 
         private void btnDeleteO_Click(object sender, RoutedEventArgs e)
@@ -261,7 +267,8 @@ namespace Neag_Andreea_Lab5
                 case "Orders":
                     break;
             }
-            ReInitialize();
+           
+
         }
         private void SaveOrders()
         {
@@ -290,8 +297,7 @@ namespace Neag_Andreea_Lab5
                     MessageBox.Show(ex.Message);
                 }
             }
-            else
-if (action == ActionState.Edit)
+            else if (action == ActionState.Edit)
             {
                 dynamic selectedOrder = ordersDataGrid.SelectedItem;
                 try
@@ -354,6 +360,30 @@ if (action == ActionState.Edit)
                                  inv.Color
                              };
             customerOrdersVSource.Source = queryOrder.ToList();
+        }
+
+        
+        private void SetValidationBinding()
+        {
+            Binding firstNameValidationBinding = new Binding();
+            firstNameValidationBinding.Source = customerVSource;
+            firstNameValidationBinding.Path = new PropertyPath("FirstName");
+            firstNameValidationBinding.NotifyOnValidationError = true;
+            firstNameValidationBinding.Mode = BindingMode.TwoWay;
+            firstNameValidationBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            //string required
+            //string required
+            firstNameValidationBinding.ValidationRules.Add(new StringNotEmpty());
+            firstNameTextBox.SetBinding(TextBox.TextProperty,firstNameValidationBinding);
+            Binding lastNameValidationBinding = new Binding();
+            lastNameValidationBinding.Source = customerVSource;
+            lastNameValidationBinding.Path = new PropertyPath("LastName");
+            lastNameValidationBinding.NotifyOnValidationError = true;
+            lastNameValidationBinding.Mode = BindingMode.TwoWay;
+            lastNameValidationBinding.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
+            //string min length validator
+            lastNameValidationBinding.ValidationRules.Add(new StringMinLengthValidator());
+            lastNameTextBox.SetBinding(TextBox.TextProperty,lastNameValidationBinding); //setare binding nou
         }
     }
 }
