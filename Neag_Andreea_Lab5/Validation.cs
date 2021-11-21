2b49ebb1d24bdb4f0af55b4cf22c55449f67b19e
Andreea Neag <neag_andreea@yahoo.com>
2021-11-21 14:40:57 +0200

    lab7

--- Neag_Andreea_Lab5/Validation.cs
+++ Neag_Andreea_Lab5/Validation.cs
@@ -13,7 +13,7 @@ namespace Neag_Andreea_Lab5
         //mostenim din clasa ValidationRule
         //suprascriem metoda Validate ce returneaza un
         //ValidationResult
-        public override ValidationResult Validate(object value,System.Globalization.CultureInfo cultureinfo)
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
         {
             string aString = value.ToString();
             if (aString == "")
@@ -24,12 +24,12 @@ namespace Neag_Andreea_Lab5
     //validator pentru lungime minima a string-ului
     public class StringMinLengthValidator : ValidationRule
     {
-        public override ValidationResult Validate(object value,System.Globalization.CultureInfo cultureinfo)
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
         {
             string aString = value.ToString();
             if (aString.Length < 3)
                 return new ValidationResult(false, "String must have at least 3 characters!");
-        return new ValidationResult(true, null);
+            return new ValidationResult(true, null);
         }
     }
 
